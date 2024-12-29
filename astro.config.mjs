86824063a9f88dfd419557c129e96f0801f3e2cd
amrbashir <github@amrbashir.me>
2024-12-29 05:06:15 +0200

    feat: unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from "astro/config";
-import Icons from "unplugin-icons/vite";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [
-			Icons({
-				compiler: "astro",
-			}),
-		],
-	},
+	integrations: [UnoCSS()],
 });
---
+++ uno.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetUno, transformerVariantGroup, presetIcons } from "unocss";
+
+export default defineConfig({
+	presets: [
+		presetUno(),
+		presetIcons({
+			scale: 1.2,
+		}),
+	],
+	transformers: [transformerVariantGroup()],
+	shortcuts: {
+		"btn-link": `color-current decoration-none flex items-center gap-1 rd-[500rem]
+					 opacity-50 hover:opacity-100 focus-visible:opacity-100 transition-opacity-300`,
+	},
+});
