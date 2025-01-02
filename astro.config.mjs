8eed44dde7ca8706b06073addac47d4a48f80338
amrbashir <github@amrbashir.me>
2025-01-02 04:56:41 +0200

    feat: add go back button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [UnoCSS(), mdx()],
+	site: "https://amrbashir.me",
 });
