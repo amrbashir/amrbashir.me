ee35653738430a5317d288c69376511a3d84ea93
amrbashir <github@amrbashir.me>
2025-01-04 05:41:40 +0200

    feat: add CV

--- src/pages/cv.md
+++ src/pages/cv.md
@@ -3,6 +3,76 @@ layout: "@layouts/MDLayout.astro"
 title: "CV"
 ---
 
-# CV
 
-Coming soon...
+<a class="print:hidden opacity-50! hover:opacity-100!" href="/cv.pdf" download="Amr Bashir - CV">
+	<span class="i-ri-arrow-down-line"></span> Download as PDF
+</a>
+
+<img src="/portrait.webp" alt="portrait" width="150" height="150" style="border-radius: 50%" class="ml-2px" align="right"/>
+
+
+# Amr Bashir
+
+> Full-stack Web and Desktop Software Engineer
+
+> Passionate about customizing systems and exploring new technologies,
+> continually seeking to innovate and expand my expertise.
+
+### Contact information
+
+- Website: https://amrbashir.me
+- E-mail: contact@amrbashir.me
+- Location: [Tanta, Egypt](https://maps.app.goo.gl/w8qBz34YJ8f9XPMH8)
+
+### Work Experience
+
+- [CrabNebula](https://crabnebula.dev/) (_Nov 2022 - Nov 2024_) - Senior Software Engineer:
+
+	- Maintained the Tauri project as part of the R&D team ensuring robust performance and stability.
+      Gaining a lot of knowledge about operating system APIs especially Windows, X11 and GTK.
+
+	- Worked on a scalable cloud platform using Rust and MongoDB in the backend and SolidJS in the frontend.
+
+- Freelancing POS mobile app (_Mar 2022 - Jun 2022_):
+
+	- Developed a Flutter Point of Sale (POS) system/application with an admin dashboard for iOS and Android.
+	- The system included local server, database and communication between different devices.
+
+### Open-source Experience
+
+- [Tauri](https://tauri.app) (_Since 2021_) -  Working-group member and contributor
+- [Hijri Widget](https://github.com/amrbashir/hijri-widget) - Android widget application for displaying The Hijri date.
+
+### Skills
+
+##### _Programming Languages_
+
+Rust, JavaScript/TypeScript, PowerShell, Bash, Kotlin, C/C++, Python, Dart
+
+##### _Back-end Technologies_
+
+Axum, MongoDB, SQLite, Docker, Node.js, Deno, GitHub Actions, Cloudflare Workers
+
+##### _Front-end Frameworks_
+
+Vue.js, SolidJS, React, Svelte
+
+##### _Desktop applications Technologies_
+
+Tauri, Electron, Win32, GTK
+
+##### _Design Skills_
+
+Figma, Adobe Photoshop, Adobe Illustrator, Inkscape
+
+### Education
+
+- Bachelor in English Literature (_2016 - 2020_)
+
+	Faculty of Languages ​and Translation, Al-Azhar University
+
+
+### Languages
+
+- Arabic (Native)
+- English (Fluent)
--- uno.config.ts
+++ uno.config.ts
@@ -6,6 +6,62 @@ import {
 	transformerDirectives,
 	presetIcons,
 } from "unocss";
+import { FileSystemIconLoader } from "@iconify/utils/lib/loader/node-loaders";
+
+const icons = () =>
+	presetIcons({
+		extraProperties: {
+			display: "inline-block",
+			height: "1.2em",
+			width: "1.2em",
+			"vertical-align": "text-bottom",
+		},
+		collections: {
+			myicons: FileSystemIconLoader("./src/assets/icons"),
+
+			devicon: () => import("@iconify-json/devicon").then((i) => i.default as any),
+			ri: () => import("@iconify-json/ri").then((i) => i.default as any),
+			"simple-icons": () =>
+				import("@iconify-json/simple-icons").then((i) => i.default as any),
+			"skill-icons": () => import("@iconify-json/skill-icons").then((i) => i.default as any),
+		},
+	});
+
+const typography = () =>
+	presetTypography({
+		cssExtend: {
+			"h1,h2,h3,h4,h5,h6": {
+				color: "black",
+				"font-weight": "800",
+			},
+			"html.dark h1, html.dark h2, html.dark h3, html.dark h4, html.dark h5, html.dark h6": {
+				color: "white",
+			},
+			strong: {
+				color: "black",
+			},
+			"html.dark strong": {
+				color: "white",
+			},
+			a: {
+				color: "black",
+				"text-decoration": "none",
+				transition: "border-bottom-color 300ms ease-in-out",
+				"border-bottom": "0.5px solid",
+				"border-bottom-color": "#55555542",
+			},
+			"a:hover": {
+				"border-bottom-color": "#000",
+			},
+			"html.dark a": {
+				color: "white",
+				"border-bottom-color": "#cdcdcd42",
+			},
+			"html.dark a:hover": {
+				"border-bottom-color": "#fff",
+			},
+		},
+	});
 
 export default defineConfig({
 	theme: {
@@ -19,49 +75,5 @@ export default defineConfig({
 					   transition-color-400 opacity-60 hover:opacity-100`,
 	},
 	transformers: [transformerVariantGroup(), transformerDirectives()],
-	presets: [
-		presetUno(),
-		presetIcons({
-			extraProperties: {
-				display: "inline-block",
-				height: "1.2em",
-				width: "1.2em",
-				"vertical-align": "text-bottom",
-			},
-		}),
-		presetTypography({
-			cssExtend: {
-				h1: {
-					color: "black",
-					"font-weight": "800",
-				},
-				"html.dark h1": {
-					color: "white",
-				},
-				strong: {
-					color: "black",
-				},
-				"html.dark strong": {
-					color: "white",
-				},
-				a: {
-					color: "black",
-					"text-decoration": "none",
-					transition: "border-bottom-color 300ms ease-in-out",
-					"border-bottom": "0.5px solid",
-					"border-bottom-color": "#55555542",
-				},
-				"a:hover": {
-					"border-bottom-color": "#000",
-				},
-				"html.dark a": {
-					color: "white",
-					"border-bottom-color": "#cdcdcd42",
-				},
-				"html.dark a:hover": {
-					"border-bottom-color": "#fff",
-				},
-			},
-		}),
-	],
+	presets: [presetUno(), icons(), typography()],
 });
