c1d6270bec84263a0a72189a0795bb7ade529b1e
amrbashir <github@amrbashir.me>
2026-01-27 15:10:54 +0200

    feat: enhance RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,9 +9,27 @@ posts = posts.sort(
 
 export const GET = () =>
 	rss({
-		title: "Amr Bashir's Blog",
-		description: "Amr Bashir's personal blog.",
+		title: "Amr Bashir",
+		description: "Amr Bashir's blog about programming and computers.",
 		site: import.meta.env.SITE,
+		customData: `
+		<link rel="self" href="/rss.xml"/>
+		<id>https://amrbashir.me</id>
+		<author>
+			<name>Amr Bashir</name>
+			<uri>https://amrbashir.me</uri>
+		</author>
+		<category term="programming"/>
+		<category term="computers"/>
+		<category term="rust"/>
+		<category term="html"/>
+		<category term="css"/>
+		<category term="javascript"/>
+		<category term="typescript"/>
+		<category term="tauri"/>
+		<category term="windows"/>
+		<category term="linux"/>
+		`,
 		items: posts.map((post) => {
 			return {
 				link: `/posts/${post.id}`,
